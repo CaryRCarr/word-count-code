54c12bc5fffef47a733e093b8bcd6f875298ab15
CaryRCarr <46456634+CaryRCarr@users.noreply.github.com>
2019-01-07 12:04:19 -0700

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -4,26 +4,30 @@ import sys
 import re
 
 def main(argv):
+	"""If only a True/False statement and a file path are passed in, then the program will return the top-three words from the given file. If a key word is passed in along with the True/False statement and file path, then the number of times that the key word appears in the file will be returned."""
 	if len(sys.argv) == 3 :
+		#Returns the top-three if there are only three arguments.
 		top_three = True
 		lower_casing = sys.argv[1]
 		input_path = sys.argv[2]
 	if len(sys.argv) == 4 :
+		#Returns the count of the number of times the key word appears if there are four arguments.
 		top_three = False
 		lower_casing = sys.argv[1]
 		input_path = sys.argv[2]
 		key_word = sys.argv[3]
 	import_file = open(input_path,"r",encoding='utf-8')
 	word_count = {}
+	removelist = "-"
+	pattern = re.compile(r'[^\w'+removelist+']')
 	for x in import_file :
 		word_list = x.split()
 		for y in word_list :
-			removelist = "-"
-			pattern = re.sub(r'[^\w'+removelist+']', '',y)
+			substitute = re.sub(pattern, '',y)
 			if lower_casing :
-				word = pattern.lower()
+				word = substitute.lower()
 			else :
-				word = pattern
+				word = substitute
 			if word in word_count :
 				word_count[word] = word_count.get(word) + 1
 			else :
